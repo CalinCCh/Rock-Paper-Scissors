d4975dca3dd11db667727a90fac4ff0b3670f096
CalinCCh <calinxirila@gmail.com>
2025-03-07 03:07:59 +0000

    Reworked rock-paper-scissors game, now with a better design!

--- javascript.js
+++ javascript.js
@@ -1,53 +1,75 @@
-
-
 let choice;
 
 let humanScore = 0;
 let computerScore = 0;
 
+const results = document.querySelector("#results");
+const finalResults = document.querySelector("#finalResults");
+const humanScoreTxt = document.querySelector("#humanScore");
+const computerScoreTxt = document.querySelector("#computerScore");
+
+
+const btnRock = document.querySelector("#rock");
+btnRock.addEventListener("click", () => {
+    choice="rock";
+    playRound(getHumanChoice(),getComputerChoice());
+
+});
+
+const btnPaper = document.querySelector("#paper");
+btnPaper.addEventListener("click", () => {
+    choice="paper";
+    playRound(getHumanChoice(),getComputerChoice());
+});
+
+const btnScissors = document.querySelector("#scissors");
+btnScissors.addEventListener("click", () => {
+    choice="scissors";
+    playRound(getHumanChoice(),getComputerChoice());
+
+});
+
+
+
 function getComputerChoice() {
-    var elements = ["rock","paper","scissors"];
-    return elements[Math.floor(Math.random()*3)].toLowerCase();
+  var elements = ["rock", "paper", "scissors"];
+  return elements[Math.floor(Math.random() * 3)].toLowerCase();
 }
 
 function getHumanChoice() {
-    return choice.toLowerCase();
+  return choice.toLowerCase();
 }
 
 function playRound(humanChoice, computerChoice) {
-    console.log("You choosed: "+humanChoice+ " || Robot choosed: "+computerChoice);
-    if (humanChoice == computerChoice) {
-        console.log("Draw!");
-    } else if ((humanChoice == "rock" && computerChoice=="paper") || (humanChoice=="paper" && computerChoice=="scissors")
-         || (humanChoice=="scissors" && computerChoice=="rock")) {
-        console.log("You lose, "+computerChoice+" beats "+humanChoice);
-        computerScore++;
-    } else {
-        console.log("You win!, "+humanChoice+" beats "+computerChoice);
-        humanScore++;
-    }
+  let result;
+  let chooses =  "You choosed: " + humanChoice + " <br><br> Robot choosed: " + computerChoice
+  if (humanChoice == computerChoice) {
+    result = "Draw!";
+  } else if (
+    (humanChoice == "rock" && computerChoice == "paper") ||
+    (humanChoice == "paper" && computerChoice == "scissors") ||
+    (humanChoice == "scissors" && computerChoice == "rock")
+  ) {
+    computerScore++;
+    result = "\nYou lose, " + computerChoice + " beats " + humanChoice;
+  } else {
+    humanScore++;
+    result = "\nYou win!, " + humanChoice + " beats " + computerChoice;
+  }
+  results.innerHTML=chooses+"<br><br>"+result;
+  if (humanScore==5) {
+    finalResults.innerHTML = "<br>You won. Congratz!";
+    humanScore=0;
+    computerScore=0;
+
+  } else if (computerScore==5) {
+    finalResults.innerHTML = "<br>You lost. :/";
+    humanScore=0;
+    computerScore=0;
+  }
+  humanScoreTxt.textContent="Human: "+humanScore;
+  computerScoreTxt.textContent="Robot: "+computerScore;
 }
-
-function playGame() {
-    for (let i=0; i<5; i++) {
-        choice = prompt("Write your choice: (rock, paper, scissors)");
-        playRound(getHumanChoice(),getComputerChoice());
-    }
-    console.log("SCORES:\nRobot: "+computerScore+"\nYou: "+humanScore);
-
-    if (humanScore>computerScore) {
-        console.log("You won the game! Congratulations.")
-    } else if (computerScore>humanScore) {
-        console.log("You lost to the machine :/")
-    } else {
-        console.log("Draw! Both have the same score!")
-    }
-}
-
-playGame();
-
-
-
 
 
 
